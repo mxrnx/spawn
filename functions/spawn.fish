098666b88c06e041765e37b3a0bda6d9d1d63f18
Knarka <knarka@users.noreply.github.com>
2017-04-01 21:58:21 +0200

    move config dir into .config/fisherman/spawn

--- functions/spawn.fish
+++ functions/spawn.fish
@@ -2,7 +2,7 @@
 
 function spawn --description 'spawn a file from template'
 	set usage 'Usage: spawn <template name> [optional new name]'
-	set spawndir (string join '' '/home/' (whoami) '/.config/spawn/')
+	set spawndir (string join '' '/home/' (whoami) '/.config/fisherman/spawn/')
 	if not test -e $spawndir
 		mkdir $spawndir
 	end
